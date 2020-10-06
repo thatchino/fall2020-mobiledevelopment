1101ddd91f95bce24309c388c6e044226f4789d8
thatchino <thvtchino@yahoo.com>
2020-10-06 16:33:53 -0400

    fixed a few files

--- week6/src/App.js
+++ week6/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Example from './components/Example/index.js'
 
 function App() {
+  Example.call(name)
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +20,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> 
     </div>
   );
 }
+
 
 export default App;
---
+++ week6/src/components/Example/index.js
@@ -0,0 +1,4 @@
+function Example() {
+	var name = "<p>Hi, my name is Alex Nguyen.</p>";
+  return name;
+}
